098c6d7138b6a592f09b365d05f7b8125b65769d
Penkwe Cjengh <498296816a@gmail.com>
2021-03-15 13:01:13 +0800

    Origin8.4 Bitmap complete

---
+++ obj/string.c.d
@@ -0,0 +1,7 @@
+obj/string.c.o: lib/string.c lib/string.h lib/stdint.h kernel/global.h \
+ lib/stdint.h kernel/debug.h
+lib/string.h:
+lib/stdint.h:
+kernel/global.h:
+lib/stdint.h:
+kernel/debug.h:
