3264a6e9e4ba0069b3d7ca9db160c99898a674d5
Penkwe Cjengh <498296816a@gmail.com>
2021-03-16 19:59:27 +0800

    Origin8.5 memory arrangement half completed

---
+++ obj/memory.c.d
@@ -0,0 +1,12 @@
+obj/memory.c.o: kernel/memory.c kernel/memory.h lib/stdint.h \
+ lib/kernel/bitmap.h kernel/global.h kernel/global.h lib/string.h \
+ lib/stdint.h kernel/debug.h lib/kernel/print.h
+kernel/memory.h:
+lib/stdint.h:
+lib/kernel/bitmap.h:
+kernel/global.h:
+kernel/global.h:
+lib/string.h:
+lib/stdint.h:
+kernel/debug.h:
+lib/kernel/print.h:
