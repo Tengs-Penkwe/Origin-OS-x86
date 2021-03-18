3446cd55542eb1b5a8a3c054ee8b345d8c1bb8cf
Penkwe Tjengh <penkwe@MacOS.local>
2021-03-18 13:06:55 +0800

    origin8 memory complete

--- obj/main.c.d
+++ obj/main.c.d
@@ -1,3 +1,9 @@
-obj/main.c.o: kernel/main.c kernel/init.h kernel/debug.h
+obj/main.c.o: kernel/main.c kernel/init.h kernel/memory.h lib/stdint.h \
+ lib/kernel/bitmap.h kernel/global.h kernel/debug.h lib/kernel/print.h
 kernel/init.h:
+kernel/memory.h:
+lib/stdint.h:
+lib/kernel/bitmap.h:
+kernel/global.h:
 kernel/debug.h:
+lib/kernel/print.h:
